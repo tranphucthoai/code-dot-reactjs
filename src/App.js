e8fb518da6c4cabe85b2ac3becd2f7ea255f8100
Tran Phuc Thoai <tranphucthoaiit@gmail.com>
2021-12-31 16:02:00 +0700

    update 31-12-16h

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Hero from "./features/Hero";
 import Strategy from "./features/Strategy";
 import Services from "./features/Services/index";
@@ -9,11 +9,18 @@ import Contact from "./features/Contact";
 import Footer from "./features/Footer";
 import Clients from "./features/Clients";
 import Navbar from "./features/Navbar";
+import clsx from "clsx";
 
 function App() {
+  const [scroll, setScroll] = useState(false);
+  useEffect(() => {
+    window.addEventListener("scroll", () => {
+      setScroll(window.scrollY > 80);
+    });
+  }, []);
   return (
     <>
-      <header>
+      <header className={clsx(scroll && "fixed-nav")}>
         <Navbar />
       </header>
       <main>
--- src/features/Navbar/index.jsx
+++ src/features/Navbar/index.jsx
@@ -1,25 +1,25 @@
 import React, { useEffect, useState } from 'react';
 import navbarApi from './../../api/navbarApi';
-
-Navbar.propTypes = {
-
-};
+import clsx from 'clsx';
 
 function Navbar() {
-
     const [data, setData] = useState({})
+    const [showMenu, setShowNenu] = useState(false)
 
     useEffect(() => {
         (async () => {
             try {
                 const respone = await navbarApi.getAll()
                 setData(...respone);
-                console.log('data', ...respone);
+                // console.log('data', ...respone);
             } catch (error) {
                 console.log('Failed to fetch api', error);
             }
         })()
     }, [])
+
+    console.log('showMenu', showMenu);
+
     return (
         <nav className="navbar">
             <div className="container">
@@ -27,7 +27,7 @@ function Navbar() {
                     <a className="navbar-brand" href="#">
                         <img src={data.logo} alt="" />
                     </a>
-                    <div className="navbar-collapse">
+                    <div className={clsx(showMenu && "active", "navbar-collapse")}>
                         <ul className="navbar-nav nav">
                             {data.nav?.map((item, index) => (
                                 <li key={item.id} className="nav-item">
@@ -41,8 +41,12 @@ function Navbar() {
                             <a href={data["btn-direction"]} className="btn bg-primary" type="submit">{data["btn-text"]}</a>
                         </form>
                     </div>
-                    <div className="navbar-toogle">
-                        <img src="./images/nav-icon.png" alt="" />
+                    <div className="navbar-toogle" onClick={() => {
+                        setShowNenu(!showMenu);
+                        console.log('showMenu', showMenu);
+
+                    }}>
+                        <img src={data["icon-toogle"]} alt="" />
                     </div>
                 </div>
             </div>
